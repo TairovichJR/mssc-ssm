37b481208d553875fde1f92c0411307fa0cb1f3a
TairovichJR <yokubov.omonjon@gmail.com>
2021-04-19 12:12:47 -0400

    enums for states and jpa

---
+++ src/main/java/guru/springframework/msscssm/domain/Payment.java
@@ -0,0 +1,26 @@
+package guru.springframework.msscssm.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentEvent state;
+
+    private BigDecimal amount;
+}
